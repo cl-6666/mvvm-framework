85f4ad17cccb2a90ddba7378b62c944f8c9d39aa
陈力 <cl_666@yeah.net>
2024-04-10 11:50:13 +0800

    使用说明修改

--- app/src/main/java/com/cl/test/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/cl/test/ui/fragment/HomeFragment.kt
@@ -6,11 +6,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.cl.test.base.BaseFragment
 import com.cl.test.databinding.FragmentHomeBinding
 import com.cl.test.ext.init
-import com.cl.test.ext.parseState
 import com.cl.test.ui.adapter.ArticleListAdapter
 import com.cl.test.ui.viewmodel.MainViewModel
-import com.hjq.toast.Toaster
+import com.maxvision.mvvm.ext.parseState
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<MainViewModel,FragmentHomeBinding>(){
@@ -32,25 +32,25 @@ class HomeFragment : BaseFragment<MainViewModel,FragmentHomeBinding>(){
                     mArticleListAdapter.submitList(it.datas)
                 }, {
                     //请求失败(网络连接问题，服务器的结果码不正确...异常都会走在这里)
-                    Toaster.show("请求失败")
+                    toast("请求失败")
                 })
             })
 
 
         //不脱壳
-        mViewModel.articleListResult2.observe(viewLifecycleOwner, Observer {resultState ->
-            parseState(resultState,{
-                if(it.errorCode==0){
+        mViewModel.articleListResult2.observe(viewLifecycleOwner) { resultState ->
+            parseState(resultState, {
+                if (it.errorCode == 0) {
                     //请求成功 打印消息
-                    Toaster.show(it.data)
-                }else{
+                    toast(it.data.toString())
+                } else {
                     //请求失败
-                    Toaster.show(it.errorMsg)
+                    toast(it.errorMsg)
                 }
-            },{
+            }, {
                 //请求发生了异常
-                Toaster.show(it.errorMsg)
+                toast(it.errorMsg)
             })
-        })
+        }
     }
 }
